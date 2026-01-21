af57151a27f2a535b50922c5f8f129ce600bed60
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-01-21 13:51:51 +0530

    mts

---
+++ money-transfer-system/src/test/java/com/boi/MoneyTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
